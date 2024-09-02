9eab4d1cf5704e8d7c132f1bbfd15868b8cb8ecb
Bob Scott <bobscott78@gmail.com>
2024-09-02 13:57:56 +0100

    Scan single item

--- src/checkout.ts
+++ src/checkout.ts
@@ -1,3 +1,12 @@
-export function total(): number {
-  return 0;
+export function checkout(): any {
+  let total = 0;
+  
+  return {
+    scan: () => {
+      total += 50;
+    },
+    total: () => {
+      return total;
+    }
+  };
 }
--- tests/checkout.test.ts
+++ tests/checkout.test.ts
@@ -1,8 +1,17 @@
-import { total } from '../src/checkout';
+import { checkout, total } from '../src/checkout';
 
 describe('Empty Basket', () => {
   it('total should be 0', () => {
-    const result = total();
+    const result = checkout().total();
     expect(result).toBe(0);
   });
+});
+
+describe('Single item', () => {
+  it('item code A should be 50', () => {
+    var c = checkout();
+    c.scan('A');
+    const total = c.total();
+    expect(total).toBe(50);
+  })
 });
